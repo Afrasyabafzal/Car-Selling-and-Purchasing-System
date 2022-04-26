59893a1ca104f4a34640c02143630d00a507a65c
Afrasyabafzal <afrasyabafzal5@gmail.com>
2022-04-26 09:58:08 +0500

    Completed the UI for Project

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -18,8 +18,36 @@ export class MenuComponent implements OnInit {
 
   constructor(private service:OrderDetailsService) { }
   carData:any;
+  carData2:any;
+  category:any;
+  Cities :any;
+  prices : any;
   ngOnInit(): void {
+    this.carData2 =  this.service.carDetails;
     this.carData = this.service.carDetails;
+    this.category = this.service.category;
+    this.Cities = this.service.city;
+    this.prices = this.service.price;
   }
-
+  public selectedBrand: any;
+  public valueSelected() {
+    this.carData = this.service.carDetails.filter(
+      item => item.name === this.selectedBrand
+    );
+  }
+  public valueSelectedCat() {
+    this.carData = this.service.carDetails.filter(
+      item => item.category === this.selectedBrand
+    );
+  }
+  public valueSelectedCity() {
+    this.carData = this.service.carDetails.filter(
+      item => item.location === this.selectedBrand
+    );
+  }
+  public valueSelectedPrice() {
+    this.carData = this.service.carDetails.filter(
+      item => (item.price <= this.selectedBrand)
+    );
+  }
 }
--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -7,49 +7,158 @@ import { Injectable } from '@angular/core';
 export class OrderDetailsService {
 
   constructor() { }
-
+  //price
+  price = [
+    10000,
+    20000,
+    30000,
+    40000,
+    50000,
+    60000,];
+  //city
+  city = [
+           "Islamabad",
+            "Lahore",
+            "Karachi",
+            "Peshawar",
+            "Quetta",
+  ];
+  //category
+  category = [
+    {
+      id: 1,
+      name: 'CAR'
+    },
+    {
+      id: 2,
+      name: 'BIKE'
+    },
+  ]
   // carDetails
   carDetails = [{
     id: 1,
     name: 'Audi',
-    price: '$12.00',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.',
+    category: this.category[0].name,
+    price: 20000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
     img: '../assets/img/car1.jpg'
 
   },
   {
+    id: 7,
+    name: 'Harley Davidson',
+    category: this.category[1].name,
+    price: 30000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '3',
+    img: '../assets/img/bike1.jpg'
+  },
+
+  {
     id: 2,
     name: 'BMW',
-    price: '$15.00',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.',
+    category: this.category[0].name,
+    price: 40000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '5',
     img: '../assets/img/car2.jpg'
+  },
+  {
+    id: 11,
+    name: 'Suzuki GSX',
+    category: this.category[1].name,
+    price: 15000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
+    img: '../assets/img/bike5.jpg'
   },
   {
     id: 3,
     name: 'Mercedes',
-    price: '$18.00',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.',
+    category: this.category[0].name,
+    price: 16000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
     img: '../assets/img/car3.jpg'
   },
   {
     id: 4,
     name: 'Nissan',
-    price: '$20.00',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.',
+    category: this.category[0].name,
+    price: 17000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
     img: '../assets/img/car4.jpg'
   },
   {
     id: 5,
     name: 'Porsche',
-    price: '$25.00',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.',
+    category: this.category[0].name,
+    price: 18000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
     img: '../assets/img/car5.jpg'
   },
   {
     id: 6,
     name: 'Toyota',
-    price: '$25.00',
-    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.',
+    category: this.category[0].name,
+    price: 19000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
     img: '../assets/img/car6.jpg'
-  }]
+  },
+  // BIKE DETAILS
+  {
+    id: 10,
+    name: 'Honda CafeRacer',
+    category: this.category[1].name,
+    price: 15000,
+    date_added: '12/12/2019',
+    location: 'Lahore',
+    rating: '4',
+    img: '../assets/img/bike4.jpg'
+  },
+  {
+    id: 8,
+    name: 'Yamaha R5',
+    category: this.category[1].name,
+    price: 20000,
+    date_added: '12/12/2019',
+    location: 'Rawalpindi',
+    rating: '5',
+    img: '../assets/img/bike2.jpg'
+  },
+
+  {
+    id: 9,
+    name: 'KTM  X-90',
+    category: this.category[1].name,
+    price: 25000,
+    date_added: '12/12/2019',
+    location: 'Islamabad',
+    rating: '4',
+    img: '../assets/img/bike3.jpg'
+  },
+  
+  {
+    id: 12,
+    name: 'Kawasaki Ninja',
+    category: this.category[1].name,
+    price: 30000,
+    date_added: '12/12/2019',
+    location: 'Faislabad',
+    rating: '4',
+    img: '../assets/img/bike6.jpg'
+  },
+]
 }
